25169bb1bc9dd380e833da15e1786174f4fc14e2
Badr Nasher <badrnasher@gmail.com>
2023-04-08 02:30:53 +0300

    Assignment Done

---
+++ index.js
@@ -0,0 +1,16 @@
+function Scooter(year, color, model) {
+  this.year = year;
+  this.color = color;
+  this.model = model;
+}
+
+function Driver(name, age, experience) {
+  this.name = name;
+  this.age = age;
+  this.experience = experience;
+}
+
+function PickupLocation(address, city) {
+  this.address = address;
+  this.city = city;
+}
